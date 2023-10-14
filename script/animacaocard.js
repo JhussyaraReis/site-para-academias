ed17723f146693d62d130a2500f4b31b9abddefa
JhussyaraReis <147738753+JhussyaraReis@users.noreply.github.com>
2023-10-14 09:40:36 -0300

    primeiro commit

---
+++ script/animacaocard.js
@@ -0,0 +1,8 @@
+
+	VanillaTilt.init(document.querySelectorAll(".destaque"), {
+		max: 25,
+		speed: 10,
+	});
+	
+	//It also supports NodeList
+	VanillaTilt.init(document.querySelectorAll(".destaque"));
